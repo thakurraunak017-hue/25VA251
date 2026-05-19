9609a814c20efdc75643170ab450512fa84c354c
kishlay kumar thakur <thakurraunak017@gmail.com>
2026-05-19 23:56:58 +0530

    kishlay1

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,25 @@
+import StudentRegistration from "./component/Studentregistration";
+
+function App() {
+  return (
+    <div>
+      {/* Demo Component
+      <Demo />
+
+      <hr />
+
+      {/* Props Example */}
+      {/* <h1 style={{ textAlign: "center" }}>Student Details</h1>
+
+      <Student name="Kishlay" age={18} course="B.Tech" />
+      <Student name="Rahul" age={19} course="BCA" />
+
+      <hr /> */} 
+
+      {/* Student Registration Form */}
+      <StudentRegistration />
+    </div>
+  );
+}
+
+export default App;
---
+++ vite-project/src/component/Props.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Student from "./Student";
+
+function App() {
+  return (
+    <div>
+      <Student name="Kishlay" age="18" course="B.Tech" />
+      <Student name="Rahul" age="19" course="BCA" />
+    </div>
+  );
+}
+
+export default App;
