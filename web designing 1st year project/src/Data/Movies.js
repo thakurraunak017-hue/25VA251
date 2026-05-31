ce700bad52cf23baff340feb2a53d999d96bad2d
kishlay kumar thakur <thakurraunak017@gmail.com>
2026-05-31 23:33:43 +0530

    Initial Netflix Clone Project

---
+++ web designing 1st year project/src/Data/Movies.js
@@ -0,0 +1,19 @@
+const movies = [
+  {
+    id: 1,
+    title: "Stranger Things",
+    image: "https://via.placeholder.com/200x300"
+  },
+  {
+    id: 2,
+    title: "Wednesday",
+    image: "https://via.placeholder.com/200x300"
+  },
+  {
+    id: 3,
+    title: "Money Heist",
+    image: "https://via.placeholder.com/200x300"
+  }
+];
+
+export default movies;
